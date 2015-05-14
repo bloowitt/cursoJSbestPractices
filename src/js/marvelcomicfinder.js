393a519d0d836542bf8363db2ae0474d181fe916
Cursos BuntPlanet <cursos@buntplanet.com>
2015-05-14 12:41:25 +0200

    controllers moved to a different file

---
+++ src/js/marvelcomicfinder.js
@@ -0,0 +1,49 @@
+
+var MarvelComicFinder = ( function( $ ) {
+  var _comics = []
+
+  function _intersect(listA, listB){
+    var listToReturn = [];
+    for (var m in listA){
+      for (var n in listB){
+        if (listA[m].getId() === listB[n].getId()){
+          listToReturn.push(listA[m]);
+        }
+      }
+    }
+    return listToReturn;
+  }
+
+  function _manageResult(data){
+    if (_comics.length === 0){
+      _comics = data;
+    } else {
+      _comics = _intersect(_comics, data);
+      MarvelComicFinderView.printComicList(_comics);
+    }
+  }
+
+  function findComics(firstChar, secondChar){
+    _comics = [];
+    api.comics(firstChar, _manageResult);
+    api.comics(secondChar, _manageResult);
+  }
+
+  function generateCharacterOptions() {
+    var myChars = api.characters()
+    MarvelComicFinderView.addCharacters(myChars);
+  }
+
+  function init(){
+    MarvelComicFinderView.setupViewElements();
+    MarvelComicFinder.generateCharacterOptions();
+  }
+
+  return {
+    generateCharacterOptions : generateCharacterOptions,
+    findComics: findComics,
+    init: init,
+    testIntersect: _intersect
+  };
+
+} )( $ );
